e9ba64db88c1d717eb2f23bda0696163be823002
Darwin Stephanus <darwin.stephanus@gmail.com>
2020-09-15 02:00:49 -0400

    Updates

---
+++ precache-manifest.7b806cf6080446be98c6007d19de421f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51013b354576ffed2653c2ebdc409db9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7b9e3a004fc5c50c5d37",
+    "url": "/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "695cbc61d131e14bd8f2",
+    "url": "/static/js/2.519af952.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.519af952.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b9e3a004fc5c50c5d37",
+    "url": "/static/js/main.07123d5a.chunk.js"
+  },
+  {
+    "revision": "ab5ed307613083267d16",
+    "url": "/static/js/runtime-main.54df86da.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.b4a3946a14b5c353b6525e9c80702776.js"
+  "/precache-manifest.7b806cf6080446be98c6007d19de421f.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-deploy/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
